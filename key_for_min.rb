ac82efece8de6a09f8879a6ac0b074ebe47d76b5
Doug K <doug.kerzner@gmail.com>
2021-01-19 19:43:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   
 name_hash.each do |key, value|
   value = original_value
-  compare = original_valuevalue
+  compare = original_value
   if value == {}
      return nil 
    end 
